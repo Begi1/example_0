87aa4a3fb3afe86b865a3ae95986abb16a75575d
Giorgi <giorgibegii@gmail.com>
2025-04-25 18:34:26 +0400

    team board

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,13 +1,131 @@
-"use client";
-
+"use client"
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Button, Box, Card, CardContent } from "@mui/material";
 import Navbar from "@/components/Navbar";
+import TodoModal from "@/components/TodoModal"; // Import TodoModal
 
-export default function Team() {
+// Interfaces for Todo
+interface Todo {
+  id: string;
+  title: string;
+  description: string;
+}
+
+const TodoBoard: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [working, setWorking] = useState<Todo[]>([]);
+  const [done, setDone] = useState<Todo[]>([]);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+
+  const addTodo = (todo: Todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const moveToWorking = (todo: Todo) => {
+    setTodos(todos.filter((t) => t.id !== todo.id));
+    setWorking([...working, todo]);
+  };
+
+  const moveToDone = (todo: Todo) => {
+    setWorking(working.filter((t) => t.id !== todo.id));
+    setDone([...done, todo]);
+  };
+
+  const moveToTodosFromWorking = (todo: Todo) => {
+    setWorking(working.filter((t) => t.id !== todo.id));
+    setTodos([...todos, todo]);
+  };
+
+  const moveToWorkingFromDone = (todo: Todo) => {
+    setDone(done.filter((t) => t.id !== todo.id));
+    setWorking([...working, todo]);
+  };
 
   return (
-    <>
+    <Box>
+      {/* Navbar */}
       <Navbar />
 
-    </>
+      <Box p={3}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <Typography variant="h6">Team</Typography>
+            <Button variant="contained" color="primary" onClick={openModal}>
+                Add Todo
+            </Button>
+        </Box>
+
+        <Box display="flex" justifyContent="space-between" mt={2}>
+          {/* Todo Section */}
+          <Box flex={1}>
+            <Typography variant="h6">Todos</Typography>
+            <Box>
+              {todos.map((todo) => (
+                <Card key={todo.id} sx={{ mb: 2 }}>
+                  <CardContent>
+                    <Typography variant="body1">{todo.title}</Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {todo.description}
+                    </Typography>
+                    <Button variant="outlined" color="primary" onClick={() => moveToWorking(todo)}>
+                      Move to Working
+                    </Button>
+                  </CardContent>
+                </Card>
+              ))}
+            </Box>
+          </Box>
+
+          {/* Working Section */}
+          <Box flex={1} sx={{ mx: 2 }}>
+            <Typography variant="h6">Working</Typography>
+            <Box>
+              {working.map((todo) => (
+                <Card key={todo.id} sx={{ mb: 2 }}>
+                  <CardContent>
+                    <Typography variant="body1">{todo.title}</Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {todo.description}
+                    </Typography>
+                    <Button variant="outlined" color="primary" onClick={() => moveToDone(todo)}>
+                      Move to Done
+                    </Button>
+                    <Button variant="outlined" color="secondary" onClick={() => moveToTodosFromWorking(todo)} sx={{ ml: 1 }}>
+                      Move to Todos
+                    </Button>
+                  </CardContent>
+                </Card>
+              ))}
+            </Box>
+          </Box>
+
+          {/* Done Section */}
+          <Box flex={1}>
+            <Typography variant="h6">Done</Typography>
+            <Box>
+              {done.map((todo) => (
+                <Card key={todo.id} sx={{ mb: 2 }}>
+                  <CardContent>
+                    <Typography variant="body1">{todo.title}</Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {todo.description}
+                    </Typography>
+                    <Button variant="outlined" color="primary" onClick={() => moveToWorkingFromDone(todo)}>
+                      Move to Working
+                    </Button>
+                  </CardContent>
+                </Card>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+      {modalOpen && <TodoModal closeModal={closeModal} addTodo={addTodo} />}
+    </Box>
   );
-}
+};
+
+export default TodoBoard;
--- components/TodoModal.tsx
+++ components/TodoModal.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button } from "@mui/material";
+
+// Interfaces for Todo
+interface Todo {
+  id: string;
+  title: string;
+  description: string;
+}
 
 interface TodoModalProps {
   closeModal: () => void;
+  addTodo: (todo: Todo) => void;
 }
 
-const TodoModal: React.FC<TodoModalProps> = ({ closeModal }) => {
+const TodoModal: React.FC<TodoModalProps> = ({ closeModal, addTodo }) => {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handleCreate = () => {
+    const newTodo: Todo = {
+      id: new Date().toISOString(),
+      title,
+      description,
+    };
+    addTodo(newTodo);
+    closeModal();
+  };
+
   return (
     <Dialog open={true} onClose={closeModal}>
       <DialogTitle>Create Todo</DialogTitle>
@@ -18,6 +39,8 @@ const TodoModal: React.FC<TodoModalProps> = ({ closeModal }) => {
           type="text"
           fullWidth
           variant="outlined"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <TextField
           margin="dense"
@@ -28,13 +51,15 @@ const TodoModal: React.FC<TodoModalProps> = ({ closeModal }) => {
           multiline
           rows={4}
           variant="outlined"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
       </DialogContent>
       <DialogActions>
         <Button onClick={closeModal} color="primary">
           Cancel
         </Button>
-        <Button onClick={closeModal} color="primary">
+        <Button onClick={handleCreate} color="primary">
           Create
         </Button>
       </DialogActions>
